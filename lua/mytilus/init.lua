95934005717dae776b4f57548a9a8f59bdef3e19
racagogi <racagogi@gmail.com>
2025-02-03 11:58:01 +0900

    fix(neovim terminal crash): remove g:terminal_colors

--- lua/mytilus/init.lua
+++ lua/mytilus/init.lua
@@ -1,27 +1,30 @@
 local M = {}
 
----@type Config
-M.configs = {
-	colors = require('mytilus.colors.mytilus_light'),
-	theme = 'mytilus-light', -- can be light, dark or mytilus_dark
-	options = {
-		sideBarDim = true, --if false then sidebar bg is same normal
-		statusBarRevers = true, --if false, statusBarRevers bg is d2_black,
-		NCWindowDim = true, --if false, not current window bg is same normal
-		constant = "none", -- or "bold", "italic"
-		str = "none",     -- or "bold", "italic"
-		func = "none",    -- or "bold", "italic"
-		type = "none",    -- or "bold", "italic"
-		statement = "none", -- or "bold", "italic"
-		keyword = "none", -- or "bold", "italic"
-	},
-	overides = {}         -- ["@string"] = { fg = "#222222", italic = true },
-}
+---@param theme? string
+---@return Config
+function M.configs(theme)
+	return {
+		colors = require('mytilus.colors.mytilus_light'),
+		theme = theme or 'mytilus-light', -- can be light, dark or mytilus_dark
+		options = {
+			sideBarDim = true, --if false then sidebar bg is same normal
+			statusBarRevers = true, --if false, statusBarRevers bg is d2_black,
+			NCWindowDim = true, --if false, not current window bg is same normal
+			constant = "none", -- or "bold", "italic"
+			str = "none", -- or "bold", "italic"
+			func = "none", -- or "bold", "italic"
+			type = "none", -- or "bold", "italic"
+			statement = "none", -- or "bold", "italic"
+			keyword = "none", -- or "bold", "italic"
+		},
+		overides = {}     -- ["@string"] = { fg = "#222222", italic = true },
+	}
+end
 
 ---@param user_config UserConfig
 function M.setup(user_config)
 	user_config = user_config or {}
-	M.configs = vim.tbl_deep_extend("force", M.configs, user_config)
+	M.configs = vim.tbl_deep_extend("force", M.configs(), user_config)
 end
 
 ---@param theme_name? string
@@ -52,24 +55,6 @@ function M.load(theme_name)
 		vim.g.colors_name = 'mytilus-light'
 		vim.o.background  = 'light'
 	end
-
-	local c                 = M.configs.colors
-	vim.g.terminal_color_0  = { fg = c.d1_black }
-	vim.g.terminal_color_8  = { fg = c.d3_black }
-	vim.g.terminal_color_7  = { fg = c.d1_white }
-	vim.g.terminal_color_15 = { fg = c.d3_white }
-	vim.g.terminal_color_1  = { fg = c.d1_red }
-	vim.g.terminal_color_9  = { fg = c.d3_red }
-	vim.g.terminal_color_2  = { fg = c.d1_green }
-	vim.g.terminal_color_10 = { fg = c.d3_green }
-	vim.g.terminal_color_3  = { fg = c.d1_yellow }
-	vim.g.terminal_color_11 = { fg = c.d3_yellow }
-	vim.g.terminal_color_4  = { fg = c.d1_blue }
-	vim.g.terminal_color_12 = { fg = c.d3_blue }
-	vim.g.terminal_color_5  = { fg = c.d1_purple }
-	vim.g.terminal_color_13 = { fg = c.d3_purple }
-	vim.g.terminal_color_6  = { fg = c.d1_cyan }
-	vim.g.terminal_color_14 = { fg = c.d3_cyan }
 
 	require("mytilus.groups").highlightgroups(M.configs)
 end
